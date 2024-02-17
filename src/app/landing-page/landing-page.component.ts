93662852bb785318f2d25dc4b446039b2f289fee
Abadom Emeka <abadomemekavictor@gmail.com>
2024-02-17 09:24:27 +0100

    initial commit

---
+++ src/app/landing-page/landing-page.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-landing-page',
+  templateUrl: './landing-page.component.html',
+  styleUrls: ['./landing-page.component.scss']
+})
+export class LandingPageComponent implements OnInit {
+
+  default_theme : string = 'theme-white';
+
+  storedTheme : string = localStorage.getItem('theme-color') ?? 'default_theme';
+
+  isMenuOpen: boolean = false;
+
+  constructor() {}
+  ngOnInit(): void {
+  }
+
+  setTheme(theme: string) {
+    localStorage.setItem('theme-color', theme);
+    this.storedTheme = localStorage.getItem('theme-color') ?? 'default-theme';
+  }
+
+  nav_toggle() {
+    this.isMenuOpen = !this.isMenuOpen
+  }
+
+  isDarkMode:boolean = false;
+
+}
